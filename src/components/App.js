9502cc938a6ea188c47a0847bfb6a4438afe6d75
Junaid Farooq <junaid@evercam.io>
2018-08-04 20:43:57 +0500

    Sign{in,up} through firebase working.

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {RouteHandler} from "react-router";
+import Login from "./Login";
 
 export default class App extends React.Component {
   state = { user: USER }
@@ -7,16 +8,17 @@ export default class App extends React.Component {
   render () {
     return <div>
       <div className='row'>
-        <div className='three colums'>
+        <div className='three columns'>
           <h1> Wicker </h1>
-          Login
+          <Login user={this.state.user} setUser={this.setUser}/>
 
           PageList
         </div>
-        <div className='nine colums'>
+        <div className='nine columns'>
           <RouteHandler user={this.state.user} />
         </div>
       </div>
     </div>;
   }
+  setUser = (user) => this.setState({ user: user });
 }
