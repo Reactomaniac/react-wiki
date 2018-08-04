dfe098be5d02b39bc6a0ae30a98ed1fb12c1838b
Junaid Farooq <junaid@evercam.io>
2018-08-04 19:25:46 +0500

    Initialized App component.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
-var app = "app";
+import React from "react";
+import Router from "react-router";
+import App from "./components/App";
 
-//commet
+const {Route} = Router;
+
+var routes = <Route handler={App}>
+</Route>;
+
+Router.run(routes, Router.HistoryLocation, Root =>
+  React.render(<Root />, document.getElementById('app')));
